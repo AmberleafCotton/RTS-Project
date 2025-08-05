a46e9fbd5eeae8f4dd22f4b7c5efc27cc33f6b3b
AmberleafCotton <cottonbitgames@gmail.com>
2025-08-05 09:06:06 +0200

    Team Component

---
+++ TeamComponent/TeamComponent.cpp
@@ -0,0 +1,29 @@
+﻿// Copyright 2025 AmberleafCotton. All rights reserved.
+#include "TeamComponent.h"
+
+UTeamComponent::UTeamComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+bool UTeamComponent::IsOwned(const UTeamComponent* OtherTeamComponent) const
+{
+	return OtherTeamComponent && OwningPlayerState == OtherTeamComponent->OwningPlayerState;
+}
+
+bool UTeamComponent::IsEnemy(const UTeamComponent* OtherTeamComponent) const
+{
+	return OtherTeamComponent &&
+		TeamSettings.TeamIndex != 0 && TeamSettings.TeamIndex != OtherTeamComponent->TeamSettings.TeamIndex;
+}
+
+bool UTeamComponent::IsAlly(const UTeamComponent* OtherTeamComponent) const
+{
+	return OtherTeamComponent &&
+		TeamSettings.TeamIndex == OtherTeamComponent->TeamSettings.TeamIndex;
+}
+
+bool UTeamComponent::IsNeutral() const
+{
+	return TeamSettings.TeamIndex == 0;
+}
---
+++ TeamComponent/TeamComponent.h
@@ -0,0 +1,74 @@
+﻿// Copyright 2025 AmberleafCotton. All rights reserved.
+#pragma once
+
+#include "TeamSettings.h"
+#include "RTS_Component.h"
+#include "TeamComponent.generated.h"
+
+UCLASS(Blueprintable, ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class FINALRTS_API UTeamComponent : public URTS_Component
+{
+	GENERATED_BODY()
+
+public:
+	UTeamComponent();
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "Team Component")
+	void InitializeTeam(const FTeamSettings InTeamSettings, APlayerState* InPlayerState);
+
+	UFUNCTION(BlueprintPure, Category = "Team Component")
+	void GetTeamInfo(FTeamSettings& OutTeamSettings, APlayerState*& OutPlayerState) const
+	{
+		OutTeamSettings = TeamSettings;
+		OutPlayerState = OwningPlayerState;
+	}
+
+	UFUNCTION(BlueprintPure, Category = "Team Component")
+	APlayerState* GetPlayerOwner() const
+	{
+		return OwningPlayerState;
+	}
+
+	UFUNCTION(BlueprintPure, Category = "Team Component")
+	FTeamSettings GetTeamSettings() const
+	{
+		return TeamSettings;
+	}
+
+	UFUNCTION(BlueprintCallable, Category = "Team Component")
+	void SetTeamSettings(const FTeamSettings& NewTeamSettings)
+	{
+		TeamSettings = NewTeamSettings;
+	}
+
+	UFUNCTION(BlueprintCallable, Category = "Team Component")
+	void SetPlayerOwner(APlayerState* NewPlayerState)
+	{
+		OwningPlayerState = NewPlayerState;
+	}
+
+	UFUNCTION(BlueprintPure, Category = "Team Component")
+	int GetTeamIndex() const
+	{
+		return TeamSettings.TeamIndex;
+	}
+
+	UFUNCTION(BlueprintPure, Category = "Team Component")
+	bool IsOwned(const UTeamComponent* OtherTeamComponent) const;
+
+	UFUNCTION(BlueprintPure, Category = "Team Component")
+	bool IsEnemy(const UTeamComponent* OtherTeamComponent) const;
+
+	UFUNCTION(BlueprintPure, Category = "Team Component")
+	bool IsAlly(const UTeamComponent* OtherTeamComponent) const;
+
+	UFUNCTION(BlueprintPure, Category = "Team Component")
+	bool IsNeutral() const;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Team Component")
+	FTeamSettings TeamSettings;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Team Component")
+	APlayerState* OwningPlayerState;
+};
